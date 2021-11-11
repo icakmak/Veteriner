c6445008197f078fdc3f509a3d4b0323532c1df9
icakmak <werduet@hotmail.com>
2021-11-11 11:32:32 +0300

    Admin Panel İçerik değişikliği

--- VeterinerApp/VetApps/admin.py
+++ VeterinerApp/VetApps/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from VetApps.models import Customer,Kind,Animal
 
-admin.site.register(Customer)
-admin.site.register(Kind)
-admin.site.register(Animal)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display =['id','customerName','customerCity','customerPhone']
+    class Meta:
+        model = Customer
+
+class AnimalAdmin(admin.ModelAdmin):
+    list_display =['id','animalName','animalBirthdate','animalCustomer','animalKind']
+    class Meta:
+        model = Animal
+
+class KindAdmin(admin.ModelAdmin):
+    list_display =['id','kindName']
+    class Meta:
+        model = Kind
+
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(Kind,KindAdmin)
+admin.site.register(Animal,AnimalAdmin)
 # Register your models here.
